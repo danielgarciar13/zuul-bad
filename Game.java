ecfcb8f3ac654ac55aa4802daac01a28ac9fa35c
Daniel Garcia Rivas <garciarivas.d99@gmail.com>
2019-05-24 12:30:04 +0200

    02-El jugador se mueve correctamente por el mapa

--- Game.java
+++ Game.java
@@ -34,23 +34,25 @@ public class Game
      */
     private void createRooms()
     {
-        Room outside, theater, pub, lab, office;
+        Room europa, africa, asia, oceania, america, antartida;
       
         // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
+        europa = new Room("You are in Europe");
+        africa = new Room("You are in Africa");
+        asia = new Room("You are in Asia");
+        oceania = new Room("You are in Oceania");
+        america = new Room("You are in america");
+        antartida = new Room("You are in Antarctica");
         
         // initialise room exits
-        outside.setExits(null, theater, lab, pub);
-        theater.setExits(null, null, null, outside);
-        pub.setExits(null, outside, null, null);
-        lab.setExits(outside, office, null, null);
-        office.setExits(null, null, null, lab);
+        europa.setExits(null, asia, africa, america);
+        africa.setExits(europa, null, null, null);
+        asia.setExits(null, null, oceania, europa);
+        oceania.setExits(asia, null, null, null);
+        america.setExits(null, europa, antartida, null);
+        antartida.setExits(america, null, null, null);
 
-        currentRoom = outside;  // start game outside
+        currentRoom = europa;  // start game outside
     }
 
     /**
