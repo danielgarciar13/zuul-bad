bc9358cec17481a079add36daef06c5ca4258992
Daniel Garcia Rivas <garciarivas.d99@gmail.com>
2019-05-28 17:26:35 +0200

    12-Mejorado el metodo printHelp de la clase Game

--- Game.java
+++ Game.java
@@ -143,11 +143,8 @@ public class Game
      */
     private void printHelp() 
     {
-        System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
-        System.out.println();
         System.out.println("Your command words are:");
-        System.out.println("   go quit help");
+        parser.showCommands();
     }
 
     /** 
@@ -184,7 +181,7 @@ public class Game
     private void look() {	
         System.out.println(currentRoom.getLongDescription());
     }
-    
+
     private void eat() {	
         System.out.println("You have eaten now and you are not hungry any more");
     }
